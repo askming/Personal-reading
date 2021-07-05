88603bbd57f399265b5789c9a7d105b845e9503b
askming <askming@gmail.com>
2021-07-05 09:22:14 -0700

    use glob to include all files in a folder

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def get_to_generate_issues(repo, dir_name, issue_number=None):
         if i.title + '.md' not in generated_issues_names
     ]
     if issue_number:
-        to_generate_issues.append(repo.get_issue(int(issue_number)))
+        to_generate_issues.append(repo.get_issue(int(issue_number))) # single issue not issues!
     return to_generate_issues
 
 
