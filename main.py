03deb6d3d5d0a83ff822c3b2da2de835832786a6
askming <askming@gmail.com>
2022-02-23 20:08:31 -0800

    clean up

--- main.py
+++ main.py
@@ -42,17 +42,6 @@ def get_save_to_dir(issue):
 
 
 def get_to_generate_issues(repo, issue_number=None):
-    # md_files = os.listdir(dir_name)
-    # generated_issues_names = md_files
-    # dir_year = os.path.normpath(dir_name).split(os.path.sep)[-1]
-
-    # to_generate_issues = [
-    #     i
-    #     for i in list(repo.get_issues())
-    #     if i.title + '.md' not in generated_issues_names and str(i.created_at)[:4] == dir_year
-    # ]
-    # if issue_number and str(repo.get_issue(int(issue_number)).created_at)[:4] == dir_year:
-    #     to_generate_issues.append(repo.get_issue(int(issue_number))) # single issue not issues! when an existing issue gets updated (e.g. with comments)
     # md file will only be updated when there is any change to an issue (i.e. issue_number is not NULL)
     # so we don't need to check if this issue already exists or not
     to_generate_issues = []
@@ -74,7 +63,7 @@ def save_issue(issue, me):
             for c in issue.get_comments():
                 if isMe(c, me):
                     f.write("\n\n---\n\n")
-                    f.write("*" + format_time(c.updated_at) + "*\n\n")
+                    f.write(f"_{format_time(c.updated_at)}_\n\n")
                     f.write(c.body)
 
 
@@ -83,18 +72,7 @@ def main(token, repo_name, issue_number=None):
     me = get_me(user)
     repo = get_repo(user, repo_name)
 
-    # issues = repo.get_issues()
-    # checked_dir = '' 
-    # for i in issues:
-    #     to_save_dir = get_to_save_dir(i)
-    #     if to_save_dir == checked_dir:
-    #         continue
-
-    #     checked_dir = to_save_dir
-    #     if not os.path.exists(to_save_dir):
-    #         os.mkdir(to_save_dir)
-
-    # identify issue(s） to be generated/updated
+    # identify issue(s) to be generated/updated
     to_generate_issues = get_to_generate_issues(repo, issue_number)
 
     # save md files to the to_save_dir
